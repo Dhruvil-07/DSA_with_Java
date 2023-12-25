8628fe6a71b95cba9e3988e7d858283a9316bdc6
Dhruvil Shah <dhruvilshah732@gmail.com>
2023-12-25 21:36:07 +0530

    binary tree  creation completed

---
+++ Tree/binarytree1.java
@@ -0,0 +1,81 @@
+//binary tree creation and travels
+//binary tree creation by user input using links
+//inorder , preorder , postorder travels
+
+import java.util.Scanner;
+
+class BinaryTreeStructure
+{
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to binary tree programe....");
+        System.out.println("if you want to enter child , enter data");
+        System.out.println("if you dont want to enter child , enter -1");
+
+        //BinaryTree class obj 
+        BinaryTree b = new BinaryTree();
+
+        //create binary tree
+        Node root = b.create();
+
+        System.out.println(root.data);
+    }
+}
+
+ //tree's node structure
+  class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+
+///binary tree class
+class BinaryTree
+{
+    
+    //Scanner class object
+    Scanner sc = new Scanner(System.in);
+
+    //tree creation
+    Node create()
+    {
+        //cerate null node
+        Node root = null;
+        
+        //get input for user
+        System.out.println("Enter data : ");
+        int data = sc.nextInt();
+
+        //if user enter -1  , go back
+        if(data == -1)
+        {
+            return null;
+        }
+        else
+        {
+            //if user enter data then create node 
+            root = new Node(data);
+
+            //ask for  left child
+            System.out.println("Enter value for left child of : " + root.data);
+            root.left = create();
+
+            //ask for right child
+            System.out.println("Enter value for right child of root : " + root.data);
+            root.right = create();
+        }
+        return root;
+    }
+
+
+}
