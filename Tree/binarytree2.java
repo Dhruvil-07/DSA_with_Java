5fdb9d2d818cd10ca280a67a96bd48f96dcfbab9
Dhruvil Shah <dhruvilshah732@gmail.com>
2023-12-26 00:27:52 +0530

    binary tree creation and travels by static values

---
+++ Tree/binarytree2.java
@@ -0,0 +1,145 @@
+//binary tree creation and travels
+//binary tree creation by static value using links
+//inorder , preorder , postorder travels
+
+class binarytree2 
+{
+    public static void main(String[] args) {
+        
+        //tress values
+        int a[] = new int[]{10,20,30,-1,-1,40,-1,-1,50,60,-1,-1,70,-1,-1};
+
+        //binary tree object
+        binarytree b = new binarytree();
+
+        //creatio of binar tree
+        Node  root = b.create(a);
+
+        //print rooot node
+        System.out.println(root.data);
+
+        //inorder tavels 
+        System.out.println();
+        System.out.println("Inordder travels : ");
+        b.inorder(root);
+       
+
+        //pre order travels 
+        System.out.println();
+        System.out.println("Preorder Travels  : ");
+        b.preorder(root);
+
+        
+        //post order Travels
+        System.out.println();
+        System.out.println("Post oreder Travels : ");
+        b.postorder(root);
+    }
+}
+
+
+
+//Node class structure
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right= null;
+    }
+}
+
+
+
+//binary tree sructure class
+class binarytree
+{
+    
+    int index;
+
+    binarytree()
+    {
+        index = -1;
+    }
+        
+    //tree creation
+    Node create(int[] a)
+    {
+        //increase index for next value
+        index++;
+
+        //create null node
+        Node root = null;
+
+        if(a[index] == -1)
+        {
+            return null;
+        }
+        else
+        {
+            //create node
+            root = new Node(a[index]);
+
+            //create left child
+            root.left = create(a);
+        
+            //create right child
+            root.right = create(a);
+        }
+        return root;
+    }
+
+
+    //inorder travels
+    void inorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        else
+        {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    //preorder travels
+    void preorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        else
+        {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+           
+        }
+    } 
+
+    //postorder travles
+    void postorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        else
+        {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+
+}
