2b25786b85646c11ca11dd22ecc4bc39c0ec1fb6
Dhruvil Shah <dhruvilshah732@gmail.com>
2023-12-29 08:25:53 +0530

    binary serch with iterative approch

--- search algo/array_binary_serch.java
+++ search algo/array_binary_serch.java
@@ -42,6 +42,48 @@ class array_binary_serch
         return;
     }
 
+    //iterative approch 
+    static void search2(int arr[] , int l , int u , int key)
+    {
+        if(l == u)
+        {
+            if(arr[l] == key)
+            {
+                System.out.println("Element found...");
+                return;
+            }
+            else
+            {
+                System.out.println("Element not fund...");
+                return;
+            }
+        }
+        else
+        {
+            while(l<=u)
+            {
+                  int mid = (l+u)/2;
+
+                  if(arr[mid] == key)
+                  {
+                    System.out.println("element found...");
+                    return;
+                  }
+                  else if(key > arr[mid])
+                  {
+                    l = mid+1;
+                  }
+                  else
+                  {
+                    u = mid-1;
+                  }
+            }
+            System.out.println("not found");
+            return;
+        }
+    }
+
+
 
     public static void main(String[] args) 
     {
@@ -52,6 +94,7 @@ class array_binary_serch
         //multiople values
         int b[] = {1,2,3,4,5,6,7};
         search(b, 0, b.length-1, 90);
-        
+        search2(b, 0, b.length-1, 90);
+
     }
 }
