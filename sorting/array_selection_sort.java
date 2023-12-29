66efca7b4d0e96a5b6d1e5538dad1a47daa1fb5f
Dhruvil Shah <dhruvilshah732@gmail.com>
2023-12-29 22:35:45 +0530

    insertion asc and desc sort

--- sorting/array_selection_sort.java
+++ sorting/array_selection_sort.java
@@ -11,21 +11,10 @@ import java.util.Scanner;
 
 class selection_sort_on_array
  {
-    public static void main(String[] args) 
-    {
-        //unsorted array
-        int a[] = {3,5,2,6,8,1};
 
-        //show unsorted array
-        System.out.println("Unsorted array : ");
-        for(int i = 0 ; i<a.length ; i++)
-        {
-            System.out.print(a[i] +" ");
-        }
-        
-        System.out.println();
-
-        //logic for insertion sort
+    //sort in asendin order
+    static int[] insertion_acending(int a[])
+    {       
         int smallest; //for store smallest values's index
         int temp;     // for store temp values for swaping
         for(int i = 0 ; i<a.length ; i++)
@@ -45,13 +34,72 @@ class selection_sort_on_array
             a[i] = a[smallest];
             a[smallest] = temp;
         }
+        return a;
+    }
 
 
-        //show sorted array
-        System.out.println("Sorted array : ");
+
+    //sort indecending order
+     static int[] insertion_desending(int a[])
+    {       
+        int gretest; //for store gretest values's index
+        int temp;     // for store temp values for swaping
         for(int i = 0 ; i<a.length ; i++)
         {
-            System.out.print(a[i] + " ");
+            gretest = i;
+
+            for(int j = i+1 ; j<a.length ; j++ )
+            {
+                if(a[j] > a[gretest])
+                {
+                   gretest= j;
+                }
+            }
+
+            //swap values
+            temp = a[i];
+            a[i] = a[gretest];
+            a[gretest] = temp;
+        }
+        return a;
+    }
+
+
+
+    public static void main(String[] args) 
+    {
+        //unsorted array
+        int a[] = {3,5,2,6,8,1};
+
+        //sorted array variable 
+        int asc[];
+        int desc[];
+
+        //show unsorted array
+        System.out.println("Unsorted array : ");
+        for(int i = 0 ; i<a.length ; i++)
+        {
+            System.out.print(a[i] +" ");
+        }
+        
+        System.out.println();
+
+        //show sorted array (asecnding order)
+        asc = insertion_acending(a);
+        System.out.println("ascending order sorted  array : ");
+        for(int i = 0 ; i<asc.length ; i++)
+        {
+            System.out.print(asc[i] + " ");
+        }
+
+        System.out.println();
+
+        //show sorted array (asecnding order)
+        desc = insertion_acending(a);
+        System.out.println("Decending order Sorted array : ");
+        for(int i = 0 ; i<desc.length ; i++)
+        {
+            System.out.print(desc[i] + " ");
         }
 
     }
