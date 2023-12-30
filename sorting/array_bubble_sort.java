2262d2644260737d32ad72e1fb3c68f7c3902caf
Dhruvil Shah <dhruvilshah732@gmail.com>
2023-12-30 11:29:13 +0530

    array bubble sort asc and desc order

--- sorting/array_bubble_sort.java
+++ sorting/array_bubble_sort.java
@@ -7,88 +7,89 @@
  time complexity = O(n^2);
  */
 
-class array_bubble_sort
-{
-
-    static int[] bubble_ascending(int a[])
-    {
-        int temp; //for store value temporary during swaping
-
-        for(int i = 0 ; i<a.length ; i++)
-        {
-            for(int j = 0 ; j<a.length-1-i ;j++)
-            {
-                //check and swap 
-                if(a[j] > a[j+1])
-                {
-                    temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
-                }
-            }
-        }
-        return a;
-    }
+ class array_bubble_sort
+ {
  
-
-    static int[] bubble_decending(int a[])
-    {
-        int temp; //for store value temporary during swaping
-
-        for(int i = 0 ; i<a.length ; i++)
-        {
-            for(int j = 0 ; j<a.length-1-i ;j++)
-            {
-                //check and swap 
-                if(a[j] < a[j+1])
-                {
-                    temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
-                }
-            }
-        }
-        return a;
-    }
-
-
-
-    public static void main(String[] args) 
-    {
-         //unsorted array
-         int a[] = {3,5,2,6,8,1};
+     static int[] bubble_ascending(int a[])
+     {
+         int temp; //for store value temporary during swaping
  
-         //sorted array variable 
-         int asc[];
-         int desc[];
- 
-         //show unsorted array
-         System.out.println("Unsorted array : ");
          for(int i = 0 ; i<a.length ; i++)
          {
-             System.out.print(a[i] +" ");
+             for(int j = 0 ; j<a.length-1-i ;j++)
+             {
+                 //check and swap 
+                 if(a[j] > a[j+1])
+                 {
+                     temp = a[j];
+                     a[j] = a[j+1];
+                     a[j+1] = temp;
+                 }
+             }
          }
-         
+         return a;
+     }
+  
+ 
+     static int[] bubble_decending(int a[])
+     {
+         int temp; //for store value temporary during swaping
+ 
+         for(int i = 0 ; i<a.length ; i++)
+         {
+             for(int j = 0 ; j<a.length-1-i ;j++)
+             {
+                 //check and swap 
+                 if(a[j] < a[j+1])
+                 {
+                     temp = a[j];
+                     a[j] = a[j+1];
+                     a[j+1] = temp;
+                 }
+             }
+         }
+         return a;
+     }
+ 
+ 
+ 
+     public static void main(String[] args) 
+     {
+          //unsorted array
+          int a[] = {3,5,2,6,8,1};
+  
+          //sorted array variable 
+          int asc[];
+          int desc[];
+  
+          //show unsorted array
+          System.out.println("Unsorted array : ");
+          for(int i = 0 ; i<a.length ; i++)
+          {
+              System.out.print(a[i] +" ");
+          }
+          
+          System.out.println();
+ 
+          //array sortedin ascending order
+          System.out.println("acesndig sorting : ");
+          asc = bubble_ascending(a);
+         for(int i = 0 ; i<asc.length ;i++)
+         {
+             System.out.print(asc[i] + " ");
+         }
+ 
          System.out.println();
-
-         //array sortedin ascending order
-         System.out.println("acesndig sorting : ");
-         asc = bubble_ascending(a);
-        for(int i = 0 ; i<asc.length ;i++)
-        {
-            System.out.print(asc[i] + " ");
-        }
-
-        System.out.println();
-
-        //array sorted in desceding order
-        System.out.println("Decending sorting : ");
-        desc = bubble_decending(a);
-        for(int i = 0 ; i<desc.length ;i++)
-        {
-            System.out.print(desc[i] + " ");
-        }
-
-
-    }
-}
+ 
+         //array sorted in desceding order
+         System.out.println("Decending sorting : ");
+         desc = bubble_decending(a);
+         for(int i = 0 ; i<desc.length ;i++)
+         {
+             System.out.print(desc[i] + " ");
+         }
+ 
+ 
+     }
+ }
+ 
