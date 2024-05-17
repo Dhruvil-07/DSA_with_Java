23a1f3bef653d2258d207935be24e906278ce5ef
Dhruvil Shah <dhruvilshah732@gmail.com>
2024-05-17 13:56:20 +0530

    q27->remove element

---
+++ leetcode_questions/Array/remove_element.java
@@ -0,0 +1,82 @@
+/*
+ leatcode q27 -> remove element
+
+ DESC :-
+ --------------
+ Given an integer array nums and an integer val, remove all occurrences 
+ of val in nums in-place. The order of the elements may be changed. 
+ Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, 
+to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the 
+elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+
+EXAMPLE :-
+--------------------
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums 
+being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements
+ of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+ */
+
+public class remove_element {
+    
+    public static int removeElement(int[] nums, int val) 
+    {
+        int k;
+
+        if(nums.length == 0)
+        {
+            k = 0;
+        }
+        else
+        {
+            int f = 0; 
+            int r = nums.length -1;
+    
+            while (f <= r) {
+                
+                if(nums[f] != val)
+                {
+                    f++;
+                }
+                else if( (nums[f]==val) && (nums[r] == val) )
+                {
+                    r--;
+                }
+                else if( (nums[f] == val) && (nums[r] != val ))
+                {
+                    nums[f] = nums[r];
+                    nums[r] = val;
+                }
+            }
+            k = f;   
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nusm = {3};
+        int k = removeElement(nusm, 3);
+        System.out.println(k);
+    }
+
+}
