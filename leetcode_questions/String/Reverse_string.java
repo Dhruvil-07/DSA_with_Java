f1e111624b2d4cc62d4fa72ee41f6b170536b274
Dhruvil Shah <dhruvilshah732@gmail.com>
2024-05-23 17:31:41 +0530

    q.151->Reverse Words in a String

---
+++ leetcode_questions/String/Reverse_string.java
@@ -0,0 +1,67 @@
+/*
+ q.151->Reverse Words in a String
+
+ DESC:-
+ --------------
+ Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s 
+will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. 
+The returned string should only 
+have a single space separating the words. Do not include any extra spaces.
+
+EXAMPLE:-
+----------------------
+Example 1:
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+
+Example 2:
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+
+Example 3:
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a 
+single space in the reversed string.
+ */
+
+
+public class Reverse_string {
+
+    public static String reverseWords(String s) {
+            String result = "";
+    
+            int i = s.length() - 1;
+            int j;
+    
+            while (i > -1) 
+            {
+                while ( i>-1 && s.charAt(i) == ' ') i--;
+    
+                if(i == -1) break;
+    
+                j = i+1;
+    
+                while (i>-1 && s.charAt(i) != ' ') i--;
+    
+                if(result.isEmpty())
+                    result = result.concat(s.substring(i+1, j));
+                else
+                   result = result.concat(" " + s.substring(i+1, j));
+                
+            }
+            return result;
+    }
+
+    public static void main(String[] args) {
+        String temp = "  hello world  ";
+        System.out.println(reverseWords(temp));
+    }
+}
